76570273b38c9be8d9b023bb4ef7b9f6893d8fae
Pallavi024 <pallavibishnoi24@gmail.com>
2019-04-08 14:15:01 +0530

    hello

---
+++ Desktop/HappyMenses/app/src/main/java/com/example/root/happymenses/CycleTracker.java
@@ -0,0 +1,13 @@
+package com.example.root.happymenses;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CycleTracker extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cycle_tracker);
+    }
+}
